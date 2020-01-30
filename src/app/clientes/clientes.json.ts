38ee52e6133aef73ae5679d4716124ef39f05436
Silvia Zaragoza <alejandra227@gmail.com>
2020-01-29 22:03:58 -0600

    initial commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] =[
+  {id: 1,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 2,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 3,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 4,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 5,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 6,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 7,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 8,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'},
+  {id: 9,nombre:'Chivis', apellido:'Zaragoza', createAt:'2020-01-25', email:'alejandra227@gmail.com'}
+];
